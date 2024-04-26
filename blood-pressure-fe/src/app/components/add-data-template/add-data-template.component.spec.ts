5d9bc7318d14b9aa3c7cb2dc405ab9c539d77ab1
csabafulophtec <csaba.fulop@htecgroup.com>
2024-04-26 13:22:28 +0200

    #4 | added some template-driven tests

--- blood-pressure-fe/src/app/components/add-data-template/add-data-template.component.spec.ts
+++ blood-pressure-fe/src/app/components/add-data-template/add-data-template.component.spec.ts
@@ -9,6 +9,7 @@ describe('AddDataTemplateComponent', () => {
   let fixture: ComponentFixture<AddDataTemplateComponent>;
 
   const initialState = {};
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -26,5 +27,90 @@ describe('AddDataTemplateComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display "Minimum value is (50)." if the systolic value is less than 50', () => {
+    const sysInput = fixture.nativeElement.querySelector(
+      'input[name="sys"]'
+    ) as HTMLInputElement;
+
+    sysInput.value = '10';
+    sysInput.dispatchEvent(new Event('input'));
+    sysInput.dispatchEvent(new Event('blur'));
+    fixture.detectChanges();
+
+    const sysMinError = fixture.nativeElement.querySelector('#sys-min-error');
+
+    expect(sysMinError.textContent).toContain('Minimum value is (50).');
+    expect(sysMinError).not.toBeNull();
+    expect(sysMinError.classList).toContain('invalid-feedback');
+  });
+
+  it('should display "Maximum value is (250)." if the systolic value is greater than 250', () => {
+    const sysInput = fixture.nativeElement.querySelector(
+      'input[name="sys"]'
+    ) as HTMLInputElement;
+
+    sysInput.value = '260';
+    sysInput.dispatchEvent(new Event('input'));
+    sysInput.dispatchEvent(new Event('blur'));
+    fixture.detectChanges();
+
+    const sysMaxError = fixture.nativeElement.querySelector('#sys-max-error');
+
+    expect(sysMaxError.textContent).toContain('Maximum value is (250).');
+    expect(sysMaxError).not.toBeNull();
+    expect(sysMaxError.classList).toContain('invalid-feedback');
+  });
+
+  it('should display "Sys is required" if we click into the input field and then out without entering anything', () => {
+    const sysInput = fixture.nativeElement.querySelector(
+      'input[name="sys"]'
+    ) as HTMLInputElement;
+
+    sysInput.value = '';
+    sysInput.dispatchEvent(new Event('input'));
+    sysInput.dispatchEvent(new Event('blur'));
+    fixture.detectChanges();
+
+    const sysRequired = fixture.nativeElement.querySelector(
+      '#sys-required-error'
+    );
+
+    expect(sysRequired.textContent).toContain('Sys is required');
+    expect(sysRequired).not.toBeNull();
+    expect(sysRequired.classList).toContain('invalid-feedback');
+  });
+
+  it('should display several error messages if we click the submit button without entering anything', () => {
+    const submitButton = fixture.nativeElement.querySelector(
+      '#submit-btn'
+    ) as HTMLButtonElement;
+
+    // Press the submit button
+    submitButton.click();
+    fixture.detectChanges();
+
+    const sysRequired = fixture.nativeElement.querySelector(
+      '#sys-required-error'
+    );
+    const diaRequired = fixture.nativeElement.querySelector(
+      '#dia-required-error'
+    );
+    const pulseRequired = fixture.nativeElement.querySelector(
+      '#pulse-required-error'
+    );
+
+    expect(sysRequired.textContent).toContain('Sys is required');
+    expect(sysRequired).not.toBeNull();
+    expect(sysRequired.classList).toContain('invalid-feedback');
+
+    expect(diaRequired.textContent).toContain('Dia is required');
+    expect(diaRequired).not.toBeNull();
+    expect(diaRequired.classList).toContain('invalid-feedback');
+
+    expect(pulseRequired.textContent).toContain('Pulse is required');
+    expect(pulseRequired).not.toBeNull();
+    expect(pulseRequired.classList).toContain('invalid-feedback');
   });
 });
