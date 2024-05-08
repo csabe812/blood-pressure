d7f82f75314c8947bbf5d74af9b1c23889ba104e
csabafulophtec <csaba.fulop@htecgroup.com>
2024-05-08 09:52:46 +0200

    #4 added some tests again

--- blood-pressure-fe/src/app/store/blood-pressure.selector.spec.ts
+++ blood-pressure-fe/src/app/store/blood-pressure.selector.spec.ts
@@ -1,4 +1,9 @@
-import { selectLastBloodPressureData } from './blood-pressure.selectors';
+import {
+  selectAverageDataByYear,
+  selectDataById,
+  selectLastBloodPressureData,
+  selectYearData,
+} from './blood-pressure.selectors';
 import { BlooodPressureData } from './blood-pressure.state';
 
 describe('BlooodPressureData', () => {
@@ -11,9 +16,33 @@ describe('BlooodPressureData', () => {
       other: 'Just a message',
       recorded: currentDate,
     },
-    averageData: [],
-    bloodDataByYear: [],
-    lastTenBloodPressureData: [],
+    averageData: [
+      {
+        year: 2018,
+        diaAvg: 88,
+        pulseAvg: 78,
+        sysAvg: 128,
+      },
+      {
+        year: 2019,
+        diaAvg: 89,
+        pulseAvg: 79,
+        sysAvg: 129,
+      },
+    ],
+    bloodDataByYear: [
+      { sys: 200, dia: 180, pulse: 170, other: 'Other1', recorded: new Date() },
+    ],
+    lastTenBloodPressureData: [
+      {
+        sys: 166,
+        dia: 76,
+        pulse: 86,
+        other: 'JustATest',
+        recorded: new Date(),
+        id: 166,
+      },
+    ],
     years: [],
   };
 
@@ -27,5 +56,29 @@ describe('BlooodPressureData', () => {
       other: 'Just a message',
       recorded: currentDate,
     });
+  });
+
+  it('should select avg data from 2018', () => {
+    const result = selectAverageDataByYear(2018).projector(initialState);
+    expect(result).toEqual({
+      year: 2018,
+      diaAvg: 88,
+      pulseAvg: 78,
+      sysAvg: 128,
+    });
+  });
+
+  it('should select blood pressure year data', () => {
+    const result = selectYearData({ bloodPressure: initialState });
+    expect(result.length).toEqual(1);
+    expect(result[0].sys).toEqual(200);
+  });
+
+  it('should select data by id', () => {
+    const result = selectDataById(166).projector(initialState);
+    expect(result.id).toEqual(166);
+    expect(result.sys).toEqual(166);
+    expect(result.dia).toEqual(76);
+    expect(result.pulse).toEqual(86);
   });
 });
